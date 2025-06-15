0d05f06d63e4389a70535c0cc9d28b320f7473bd
pradeep relwani <pradeep.relwani21@gmail.com>
2025-06-15 18:28:29 +0530

    refactor code

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ const port = process.env.PORT || 3000;
 
 app.get('/test', (_req: Request, res: Response) => {
   const controller = new BasketController()
-  const shoppingBasket1 = ["Apple", "Apple", "Banana", "Melon", "Melon", "Melon", "Lime", "Lime", "Lime", "Lime"];
-  const shoppingBasket2 = ["Apple", "Apple", "Banana", "Melon", "Melon", "Lime", "Lime", "Lime", "Lime", "Lime"];
-  const shoppingBasket3 = ["Apple", "Apple", "Apple", "Banana", "Banana", "Banana", "Melon", "Melon", "Lime", "Lime", "Lime", "Lime"];
+  const shoppingBasket1: string[] = ["Apple", "Apple", "Banana", "Melon", "Melon", "Melon", "Lime", "Lime", "Lime", "Lime"];
+  const shoppingBasket2: string[] = ["Apple", "Apple", "Banana", "Melon", "Melon", "Lime", "Lime", "Lime", "Lime", "Lime"];
+  const shoppingBasket3: string[] = ["Apple", "Apple", "Apple", "Banana", "Banana", "Banana", "Melon", "Melon", "Lime", "Lime", "Lime", "Lime"];
   res.json({
     shoppingBasket1: controller.calculateBasket(shoppingBasket1),
     shoppingBasket2: controller.calculateBasket(shoppingBasket2),
